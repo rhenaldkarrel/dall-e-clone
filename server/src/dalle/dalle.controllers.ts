c4115499067d3ad3a6131d026f7bc94a55c1b5ee
Rhenald <renaldkarel@gmail.com>
2023-02-02 21:52:48 +0700

    fix: dalle controller and route

--- server/src/dalle/dalle.controllers.ts
+++ server/src/dalle/dalle.controllers.ts
@@ -1,14 +1,22 @@
 import { Request, Response } from "express"
 import { Configuration, OpenAIApi } from "openai"
 
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 
 const openai = new OpenAIApi(config)
 
-export async function generateImage(res: Response, req: Request) {
+export async function generateImage(req: Request, res: Response) {
   try {
+    if (!req.body || !req.body.prompt) {
+      return res.status(400).json({ message: "Please enter your prompt correctly! "})
+    }
+
     const { prompt } = req.body
 
     const aiResponse = await openai.createImage({
@@ -23,7 +31,7 @@ export async function generateImage(res: Response, req: Request) {
     res.status(200).json({ photo: image })
   } catch (err: any) {
     console.log("Error while generating image: ", err)
-    
+
     res.status(500).send(err?.response.data.error.message)
   }
 }
--- server/src/dalle/dalle.routes.ts
+++ server/src/dalle/dalle.routes.ts
@@ -1,8 +1,5 @@
 import express from "express"
-import * as dotenv from "dotenv"
 import { generateImage } from "./dalle.controllers"
-
-dotenv.config()
 
 const router = express.Router()
 
