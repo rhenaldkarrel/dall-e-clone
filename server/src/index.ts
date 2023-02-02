bf92df17e04bb8b530381ffca3dec17ce8c26b8e
Rhenald <renaldkarel@gmail.com>
2023-02-02 20:56:24 +0700

    feat: add mongo atlas and initial routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,19 +2,31 @@ import express from "express"
 import * as dotenv from "dotenv"
 import cors from "cors"
 
+import connectDB from "./config/db"
+
+import dalleRoutes from "./dalle/dalle.routes"
+import postRoutes from "./post/post.routes"
+
 dotenv.config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json({ limit: "50mb" }))
+app.use("/api/v1/post", postRoutes)
+app.use("/api/v1/dalle", dalleRoutes)
 
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E")
 })
 
 const startServer = async () => {
-  app.listen(8080, () => console.log("Server has started on port 8080..."))
+  try {
+    connectDB(process.env.MONGODB_URL)
+    app.listen(8080, () => console.log("Server has started on port 8080..."))
+  } catch(err) {
+    console.log("Error happened when starting the server: ", err)
+  }
 }
 
 startServer()
